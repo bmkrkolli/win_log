fe420702c3182dde4ebde2df2f69fadced5426ac
B M Krishna Reddy Kolli <bmkrkolli@gmail.com>
2022-03-16 18:25:29 +0530

    v4

--- library/win_l1diag.ps1
+++ library/win_l1diag.ps1
@@ -1,14 +1,37 @@
 #!powershell
-Write-Host "OS:" (gwmi Win32_OperatingSystem).Caption;
-$cpu=gwmi win32_processor | Measure -property LoadPercentage -Average | %{$_.Average};
-$MemInfo = gwmi Win32_PerfFormattedData_PerfOS_Memory;
-$am=$MemInfo.AvailableMBytes;
-$tm=gwmi Win32_ComputerSystem | %{[Math]::Round($_.TotalPhysicalMemory/1MB)};
-$um=[Math]::Round(100-(($am/$tm)*100));
-$pageinfo=gwmi Win32_PageFileUsage;
-$pct=[Math]::Round(($pageinfo.CurrentUsage/$pageinfo.AllocatedBaseSize)*100,2);
-$lbt=gwmi win32_operatingsystem | select @{LABEL='LastBootUpTime'; EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}};
-Write-Host ";  Last Bootup Time:" $lbt.LastBootUpTime;
-Write-Host ";  CPU Utilization:" $cpu "%;  RAM Utilization:" $um "%";
-Write-Host ";  Total RAM(in MB):" $tm ";  Pagefile Utilization:" $pct "%;  Disk Utilization: ";
-GWMI Win32_LogicalDisk -Filter "DriveType='3'" | %{Write-Host $_.Name "=>" (100 - [Math]::Round(($_.FreeSpace/$_.Size)*100, 2)) "%; ";};
+#Requires -Module Ansible.ModuleUtils.Legacy
+
+$params = Parse-Args $args -supports_check_mode $true
+$check_mode = Get-AnsibleParam -obj $params -name '_ansible_check_mode' -type 'bool' -default $false
+
+try {
+    $os = get-wmiobject Win32_OperatingSystem;
+    $lbt = $os.ConverttoDateTime($os.LastBootupTime);
+    $cpu = get-wmiobject win32_processor | Measure -property LoadPercentage -Average | %{$_.Average};
+    $MemInfo = get-wmiobject Win32_PerfFormattedData_PerfOS_Memory; $am=$MemInfo.AvailableMBytes;
+    $tm = get-wmiobject Win32_ComputerSystem | %{[Math]::Round($_.TotalPhysicalMemory/1MB)};
+    $um = [Math]::Round(100-(($am/$tm)*100));
+    $pageinfo=get-wmiobject Win32_PageFileUsage;
+    $pct=[Math]::Round(($pageinfo.CurrentUsage/$pageinfo.AllocatedBaseSize)*100,2);
+    $dsk="";get-wmiobject Win32_LogicalDisk -Filter "DriveType='3'" | %{$d=$_.Name;$dsk+="[Drive:" +$d.substring(0,1)+"; Used:"+(100 - [Math]::Round(($_.FreeSpace/$_.Size)*100, 2))+"],";};
+
+    $result = @{
+        failed = $false
+        changed = $false
+        rc = 0
+        stdout = "Host:" + $os.CSName + ", OS:" + $os.Caption + ", Kernel:" + $os.Version + ", LastBootUpTime:" + ($lbt.DateTime).replace(",","") + ", CPULoad:" + $cpu + ", MemoryLoad:" + $um + ", PageFileLoad:" + $pct + ", DiskLoad:" + $dsk ;   
+    }
+}
+catch {
+
+    $result = @{
+        failed = $true
+        changed = $false
+        msg = "Failed to get Level 1 Diagnosis Information"
+        rc = 1
+        stderr = $PSItem    
+    }
+
+}
+
+Exit-Json $result;
