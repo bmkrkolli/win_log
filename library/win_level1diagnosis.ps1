3b8d87ecfccf57c54e1ec3726e2502fcbdcb2d4c
B M Krishna Reddy Kolli <bmkrkolli@gmail.com>
2022-03-16 22:13:51 +0530

    v4

--- library/win_level1diagnosis.ps1
+++ library/win_level1diagnosis.ps1
@@ -8,7 +8,7 @@ try {
     $os = get-wmiobject Win32_OperatingSystem;
     $lbt = $os.ConverttoDateTime($os.LastBootupTime);
     $cpu = get-wmiobject win32_processor | Measure -property LoadPercentage -Average | %{$_.Average};
-    $cores = get-wmiObject win32_processor;
+    $cores = get-wmiobject Win32_ComputerSystem;
     $mem = get-wmiobject Win32_PerfFormattedData_PerfOS_Memory; 
     $am = $mem.AvailableMBytes;
     $tm = get-wmiobject Win32_ComputerSystem | %{[Math]::Round($_.TotalPhysicalMemory/1MB)};
@@ -17,7 +17,7 @@ try {
     $pct = [Math]::Round(($pageinfo.CurrentUsage/$pageinfo.AllocatedBaseSize)*100,2);
     $dsk = GWMI Win32_LogicalDisk -Filter "DriveType='3'" | Select Name, @{LABEL='Used'; EXPRESSION={(100 - [Math]::Round(($_.FreeSpace/$_.Size)*100, 2))}};
     $l1 = New-Object psobject -Property @{Host = $os.CSName; OS = $os.Caption; OSArchitecture = $os.OSArchitecture;
-        LastBootUpTime = ($lbt.DateTime).replace(",",""); Cores = $cores.NumberOfCores; CPULoad = $cpu; Memory = $tm; MemoryLoad = $um; 
+        LastBootUpTime = ($lbt.DateTime).replace(",",""); Cores = $cores.NumberOfProcessors; CPULoad = $cpu; Memory = $tm; MemoryLoad = $um; 
         LogicalProcessors = $cores.NumberOfLogicalProcessors; PageFileLoad = $pct; DiskLoad = $dsk; }; 
 
     $result = @{
