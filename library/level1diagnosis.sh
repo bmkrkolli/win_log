941e1e499a3708ff3549ff052a4509cb09d71851
B M Krishna Reddy Kolli <bmkrkolli@gmail.com>
2022-03-17 12:59:57 +0530

    v4

--- library/level1diagnosis.sh
+++ library/level1diagnosis.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-hn = "$(uname -n||echo 'uname not found')"
-os = "$(egrep -w "NAME|VERSION" /etc/os-release|awk -F= '{ print $2 }'|sed 's/"//g'||echo '/etc/os-release not found')"
-kernel = "$(uname -ri||echo 'uname not found')"
-lbt = "$(uptime -s||echo 'uptime not found')"
-cpu = "$(which top >/dev/null 2>&1 && (top -b -n 2 | grep 'Cpu(s)' | tail -n 1 | awk '{print $2}'| awk -F. '{print $1}')||echo 'top command not found')"
-mem = "$(free | grep Mem | awk '{print $3/$2 * 100.0}'||echo 'free command not found')"
-swap = "$(free | grep 'Swap' | awk '{t = $2; f = $4; print (f/t)}'||echo 'free command not found')"
-fs = "$(df -TPh -x squashfs -x tmpfs -x devtmpfs | awk 'BEGIN {ORS=","} NR>1{print "[Mount:"$7", UsedPercent:"$6"]"}'||echo 'df command not found')"
+HN=$(uname -n||echo 'uname command not found')
+OS=$(egrep -w "NAME|VERSION" /etc/os-release|awk -F= '{ print $2 }'|sed 's/"//g'||echo '/etc/os-release command not found')
+KERNEL=$(uname -ri||echo 'uname command not found')
+LBT=$(uptime -s||echo 'uptime command not found')
+CPU=$(which top >/dev/null 2>&1 && (top -b -n 2 | grep 'Cpu(s)' | tail -n 1 | awk '{print $2}'| awk -F. '{print $1}')||echo 'top command not found')
+MEM=$(free | grep Mem | awk '{print $3/$2 * 100.0}'||echo 'free command not found')
+SWAP=$(free | grep 'Swap' | awk '{t = $2; f = $4; print (f/t)}'||echo 'free command not found')
+FS=$(df -TPh -x squashfs -x tmpfs -x devtmpfs | awk 'BEGIN {ORS=","} NR>1{print "[Mount:"$7", UsedPercent:"$6"]"}'||echo 'df command not found')
 
-stdoutput = "Hostname:" $hn ", OS:" $os ", Kernel:" $kernel ", LastBootUpTime:" $lbt ", CPULoadPercent:" $cpu ", MemoryLoadPercent:" $mem ", SWAPLoadPercent:" $swap ", Filesystems:" $fs
-echo "{ \"changed\": false, \"failed\": false, \"rc\": 0, \"msg\": \"\", \"stderr\": \"\", \"stdout\": \"$stdoutput\" }"
+STDOUTPUT="Hostname: "$HN", OS: "$OS", Kernel: "$KERNEL", LastBootUpTime:"$LBT", CPULoadPercent: "$CPU", MemoryLoadPercent: "$MEM", SWAPLoadPercent: "$SWAP", Filesystems: "$FS
+er="not found"
+if [[ $STDOUTPUT =~ $er ]];
+then
+    echo "{ \"changed\": false, \"failed\": true, \"rc\": 1, \"msg\": \"\", \"stderr\": \"$STDOUTPUT\", \"stdout\": \"\" }"
+else
+    echo "{ \"changed\": false, \"failed\": false, \"rc\": 0, \"msg\": \"\", \"stderr\": \"\", \"stdout\": \"$STDOUTPUT\" }"
+fi
