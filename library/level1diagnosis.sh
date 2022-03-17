e63c5f16f8515c24ee3a4e8a88f6b3a4b01a2913
B M Krishna Reddy Kolli <bmkrkolli@gmail.com>
2022-03-17 20:25:28 +0530

    v4

--- library/level1diagnosis.sh
+++ library/level1diagnosis.sh
@@ -19,6 +19,6 @@ then
     echo "{ \"changed\": false, \"failed\": true, \"rc\": 1, \"msg\": \"\", \"stderr\": {"$STDOUTPUT"}, \"stderr_lines\": {"$STDOUTPUT"}, \"stdout\": \"\", \"stdout_lines\": \"\" }"
     exit 1
 else
-    echo "{ \"changed\": false, \"failed\": false, \"rc\": 0, \"msg\": \"\", \"stderr\": \"\", \"stderr_lines\": \"\", \"stdout\": [{"$STDOUTPUT"}], \"stdout_lines\": [{"$STDOUTPUT"}] }"
+    echo "{ \"changed\": false, \"failed\": false, \"rc\": 0, \"msg\": \"\", \"stderr\": \"\", \"stderr_lines\": \"\", \"stdout\": {"$STDOUTPUT"}, \"stdout_lines\": {"$STDOUTPUT"} }"
     exit 0
 fi
